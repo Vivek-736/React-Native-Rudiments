857ac40f84934506b21b8a7c5adba86eacf6fd69
Vivek <vivekousik8555@gmail.com>
2025-04-08 20:15:51 +0530

    created all routes in the app directory for the project

---
+++ app/(mobileScreens)/index.tsx
@@ -0,0 +1,12 @@
+import { View ,Text } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl text-dark-200 font-bold">
+        Welcome to React Native!
+      </Text>
+    </View>
+  );
+}
